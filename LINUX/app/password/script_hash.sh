262eee03f24db2a65dbb33b5b4fd8509d795f6d2
landes martin <landes_martin@yahoo.fr>
2025-04-26 18:14:27 +0000

    Update script_hash.sh

--- LINUX/app/password/script_hash.sh
+++ LINUX/app/password/script_hash.sh
@@ -3,9 +3,9 @@
 # Hash originel à comparer
 read -p 'Entrer le hash :' hash
 # Dictionnaire
-fichier=
+read -p 'Entrer le chemin du fichier :' fichier
 # Type de hash utilisé pour le hash originel
-type hash=
+read -p 'Entrer le type de hash:' type_hash
 
 #Affiche le fichier, met chaque ligne dans une variable appelé line
 cat $fichier | while read line
@@ -13,7 +13,7 @@ cat $fichier | while read line
         #Créer la variable digest, qui sera comparé à hash
         #Cette variable va écrire pour chaque ligne du fichier le hash.
         #-n sypprime le retour à la ligne et awk ne va afficher que la premiere colone
-        digest=$(echo -n $line | $type hash | awk '{print $1}')
+        digest=$(echo -n $line | $type_hash | awk '{print $1}')
         if [$digest=$hash]
         then
               echo'bingo le pot de passe est :'$line
