f070f54b06b0159edb984b8f1407d1ba9e7fb38c
landes martin <landes_martin@yahoo.fr>
2025-04-26 18:09:48 +0000

    Create script_hash.sh

---
+++ LINUX/app/password/script_hash.sh
@@ -0,0 +1,24 @@
+#!bin/bash
+
+# Hash originel à comparer
+hash=
+# Dictionnaire
+fichier=
+# Type de hash utilisé pour le hash originel
+type hash=
+
+#Affiche le fichier, met chaque ligne dans une variable appelé line
+cat $fichier | while read line
+    do
+        #Créer la variable digest, qui sera comparé à hash
+        #Cette variable va écrire pour chaque ligne du fichier le hash.
+        #-n sypprime le retour à la ligne et awk ne va afficher que la premiere colone
+        digest=$(echo -n $line | $type hash | awk '{print $1}')
+        if [$digest=$hash]
+        then
+              echo'bingo le pot de passe est :'$line
+              break
+        else
+              continue
+        fi
+    done
