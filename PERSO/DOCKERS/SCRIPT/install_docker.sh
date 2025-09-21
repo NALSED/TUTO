46c3bdfcaec1d77a6f624fd1c48af58b6212dec8
landes martin <landes_martin@yahoo.fr>
2025-09-21 08:11:04 +0000

    Update install_docker.sh

--- PERSO/DOCKERS/SCRIPT/install_docker.sh
+++ PERSO/DOCKERS/SCRIPT/install_docker.sh
@@ -18,4 +18,4 @@ echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.
 
 #instalation docker
 sudo apt update
-sudo apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo apt install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
