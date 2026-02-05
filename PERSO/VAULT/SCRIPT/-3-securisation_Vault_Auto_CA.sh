3719183e10515c4cfcd25caf29c818a03f9d5777
landes martin <landes_martin@yahoo.fr>
2026-02-05 14:19:07 +0400

    Update -3-securisation_Vault_Auto_CA.sh

--- PERSO/VAULT/SCRIPT/-3-securisation_Vault_Auto_CA.sh
+++ PERSO/VAULT/SCRIPT/-3-securisation_Vault_Auto_CA.sh
@@ -2,26 +2,26 @@
 
 # === Vault_Auto + CA ===
 
-- Vault_Auto.crt
+#- Vault_Auto.crt
 sudo chmod 644 /etc/Vault/Vault_Auto/Cert/public/Vault_Auto.crt
 sudo chown vault:vault /etc/Vault/Vault_Auto/Cert/public/Vault_Auto.crt
 
-- Vault_Auto.csr 
+#- Vault_Auto.csr 
 sudo chmod 644 /etc/Vault/Vault_Auto/Cert/public/Vault_Auto.csr
 sudo chown vault:vault /etc/Vault/Vault_Auto/Cert/public/Vault_Auto.csr
 
-- Vault_Auto.key - Corriger (déjà dans private/)
+#- Vault_Auto.key - Corriger (déjà dans private/)
 sudo chmod 640 /etc/Vault/Vault_Auto/Cert/private/Vault_Auto.key
 sudo chown vault:vault /etc/Vault/Vault_Auto/Cert/private/Vault_Auto.key
 
-- CA.key
+#- CA.key
 sudo chmod 600 /etc/Vault/CA_Vault/Cert/private/CA.key
 sudo chown vault:vault /etc/Vault/CA_Vault/Cert/private/CA.key
 
-- CA.crt
+#- CA.crt
 sudo chmod 644 /etc/Vault/CA_Vault/Cert/public/CA.crt
 sudo chown vault:vault /etc/Vault/CA_Vault/Cert/public/CA.crt
 
-- CA.srl 
+#- CA.srl 
 sudo chmod 644 /etc/Vault/CA_Vault/Cert/public/CA.srl
 sudo chown vault:vault /etc/Vault/CA_Vault/Cert/public/CA.srl
