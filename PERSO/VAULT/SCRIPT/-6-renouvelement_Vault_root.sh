18308674e492770fac91b484107e8f1fe6098c83
landes martin <landes_martin@yahoo.fr>
2026-02-05 17:30:40 +0400

    Update -6-renouvelement_Vault_root.sh

--- PERSO/VAULT/SCRIPT/-6-renouvelement_Vault_root.sh
+++ PERSO/VAULT/SCRIPT/-6-renouvelement_Vault_root.sh
@@ -14,7 +14,6 @@ generation(){
         -in /etc/Vault/Vault_Root/Cert/public/Vault_Root.csr \
         -CA /etc/Vault/CA_Vault/Cert/public/CA.crt \
         -CAkey /etc/Vault/CA_Vault/Cert/private/CA.key \
-        -CAserial /etc/Vault/CA_Vault/Cert/public/CA.srl \
         -out /etc/Vault/Vault_Root/Cert/public/Vault_Root.crt \
         -days 365 \
         -sha256 \
