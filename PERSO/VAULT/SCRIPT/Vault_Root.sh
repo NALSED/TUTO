ccb2540455469a5eb067df1208b37aa3f32708b1
landes martin <landes_martin@yahoo.fr>
2026-02-05 10:42:00 +0400

    Create Vault_Root.sh

---
+++ PERSO/VAULT/SCRIPT/Vault_Root.sh
@@ -0,0 +1,23 @@
+# === Vault_Root ===
+
+sudo mkdir -p /etc/Vault/Vault_Root/Cert/{public,private}
+sudo mkdir /etc/Vault/Vault_Root/Config
+
+
+#droits
+chmod 775 /etc/Vault
+chmod 775 /etc/Vault/Vault_Root
+chmod 775 /etc/Vault/Vault_Root/Cert
+chmod 775 /etc/Vault/Vault_Root/Cert/public
+chmod 770 /etc/Vault/Vault_Root/Cert/private
+
+# propriété
+chown vault:vault /etc/Vault
+chown vault:vault /etc/Vault/Vault_Root
+chown vault:vault /etc/Vault/Vault_Root/Cert
+chown vault:vault /etc/Vault/Vault_Root/Cert/public
+chown vault:vault /etc/Vau
+
+# Héritage
+chmod g+s /etc/Vault/Vault_Root/Cert/public
+chmod g+s /etc/Vault/Vault_Root/Cert/private
