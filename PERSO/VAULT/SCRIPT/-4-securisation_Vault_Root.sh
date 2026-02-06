e1598d3d750a4130f93c6707866dda713ad944c6
landes martin <landes_martin@yahoo.fr>
2026-02-06 11:23:18 +0400

    Update -4-securisation_Vault_Root.sh

--- PERSO/VAULT/SCRIPT/-4-securisation_Vault_Root.sh
+++ PERSO/VAULT/SCRIPT/-4-securisation_Vault_Root.sh
@@ -7,13 +7,6 @@
 sudo chmod 644 /etc/Vault/Vault_Root/Cert/public/Vault_Root.crt
 sudo chown vault:vault /etc/Vault/Vault_Root/Cert/public/Vault_Root.crt
 
-
-# Vault_Root.csr
-
-sudo chmod 644 /etc/Vault/Vault_Root/Cert/public/Vault_Root.csr
-sudo chown vault:vault /etc/Vault/Vault_Root/Cert/public/Vault_Root.csr
-
-
 # Vault_Root.key 
 
 sudo chmod 640 /etc/Vault/Vault_Root/Cert/private/Vault_Root.key
