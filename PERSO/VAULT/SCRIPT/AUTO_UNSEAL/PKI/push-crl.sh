bd04da1fe2e5e751f5cfd13b7e6c15c32f2bd32a
landes martin <landes_martin@yahoo.fr>
2026-02-20 11:01:42 +0400

    Create push-crl.sh

---
+++ PERSO/VAULT/SCRIPT/AUTO_UNSEAL/PKI/push-crl.sh
@@ -0,0 +1,14 @@
+#!bin/bash
+
+curl -s https://vault.sednal.lan:8200/v1/PKI_Sednal_Root_RSA/crl \
+    -o /tmp/root_r.crl
+curl -s https://vault.sednal.lan:8200/v1/PKI_Sednal_Root_ECDSA/crl \
+    -o /tmp/root_e.crl
+curl -s https://vault.sednal.lan:8200/v1/PKI_Sednal_Inter_RSA/crl \
+    -o /tmp/intermediate_r.crl
+curl -s https://vault.sednal.lan:8200/v1/PKI_Sednal_Inter_ECDSA/crl \
+    -o /tmp/intermediate_e.crl
+
+scp /tmp/root_r.crl /tmp/root_e.crl \
+    /tmp/intermediate_r.crl /tmp/intermediate_e.crl \
+    sednal@infra.sednal.lan:/var/www/pki/
