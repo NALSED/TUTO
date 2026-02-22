3bdf08c30663fbcc4647b560db9f1da950ab3f56
landes martin <landes_martin@yahoo.fr>
2026-02-22 18:59:01 +0000

    Update renew_cert.sh

--- PERSO/VAULT/SCRIPT/PKI/renew_cert.sh
+++ PERSO/VAULT/SCRIPT/PKI/renew_cert.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
-set -e
-
-services=(proxmox pihole upsnap infra bareos-dir bareos-fd bareos-sd bareos postgresql vps cockpit)
+# Services RSA + ECDSA
+services_dual=(proxmox pihole upsnap infra vps cockpit)
+# Services RSA uniquement (Bareos)
+services_rsa=(bareos-dir bareos-fd bareos-sd-local bareos-sd-remote bareos win lin postgresql)
 base_pki="/etc/Vault/PKI"
 domain=".sednal.lan"
+
+set -e
 
 path() {
     if [[ "$1" == "proxmox" ]]; then echo "Proxmox"
@@ -12,14 +15,16 @@ path() {
     elif [[ "$1" == "upsnap" ]]; then echo "Upsnap"
     elif [[ "$1" == "infra" ]]; then echo "Infra"
     elif [[ "$1" == bareos* ]]; then echo "Bareos"
+    elif [[ "$1" == "win" ]]; then echo "Bareos"
+    elif [[ "$1" == "lin" ]]; then echo "Bareos"
     elif [[ "$1" == "postgresql" ]]; then echo "PostGreSQL"
     elif [[ "$1" == "vps" ]]; then echo "VPS"
     elif [[ "$1" == "cockpit" ]]; then echo "Cockpit"
     fi
 }
 
-# === RENOUVELLEMENT CERTIFICATS ===
-for service in "${services[@]}"; do
+# === RSA + ECDSA ===
+for service in "${services_dual[@]}"; do
     cert="${service}${domain}"
     folder=$(path "$service")
 
@@ -32,7 +37,17 @@ for service in "${services[@]}"; do
     result=$(vault write -format=json PKI_Sednal_Inter_ECDSA/issue/Cert_Inter_ECDSA common_name="$cert")
     echo "$result" | jq -r '.data.certificate' | sudo tee "$base_pki/public/$folder/Ecdsa/${service}_ecdsa.crt" > /dev/null
     echo "$result" | jq -r '.data.private_key'  | sudo tee "$base_pki/private/$folder/Ecdsa/${service}_ecdsa.key" > /dev/null
+done
 
+# === RSA UNIQUEMENT (Bareos) ===
+for service in "${services_rsa[@]}"; do
+    cert="${service}${domain}"
+    folder=$(path "$service")
+
+    echo "Renouvellement RSA : $cert → $folder"
+    result=$(vault write -format=json PKI_Sednal_Inter_RSA/issue/Cert_Inter_RSA common_name="$cert")
+    echo "$result" | jq -r '.data.certificate' | sudo tee "$base_pki/public/$folder/Rsa/${service}_rsa.crt" > /dev/null
+    echo "$result" | jq -r '.data.private_key'  | sudo tee "$base_pki/private/$folder/Rsa/${service}_rsa.key" > /dev/null
 done
 
 # Réappliquer les droits sur Vault
@@ -59,57 +74,78 @@ rsync -e ssh --no-p --chmod=F644 --chown=sednal:sednal \
 cible="sednal@192.168.0.240"
 base_bareos="/etc/bareos/ssl"
 
-ssh "$cible" "rm -f $base_bareos/Keys/*/* $base_bareos/Cert/*/*"
+ssh "$cible" "rm -f $base_bareos/Keys/dir/* $base_bareos/Cert/dir/*
+              rm -f $base_bareos/Keys/sd/local/* $base_bareos/Cert/sd/local/*
+              rm -f $base_bareos/Keys/sd/remote/* $base_bareos/Cert/sd/remote/*
+              rm -f $base_bareos/Keys/fd/* $base_bareos/Cert/fd/*
+              rm -f $base_bareos/Keys/web/* $base_bareos/Cert/web/*
+              rm -f $base_bareos/Keys/post/* $base_bareos/Cert/post/*
+              rm -f $base_bareos/Keys/client/win/* $base_bareos/Cert/client/win/*
+              rm -f $base_bareos/Keys/client/lin/* $base_bareos/Cert/client/lin/*"
 
 # DIR
 rsync -e ssh --no-p --chmod=F600 --chown=bareos:bareos \
     "$base_pki/private/Bareos/Rsa/bareos-dir_rsa.key" \
-    "$base_pki/private/Bareos/Ecdsa/bareos-dir_ecdsa.key" \
     "$cible":"$base_bareos"/Keys/dir/
 rsync -e ssh --no-p --chmod=F644 --chown=bareos:bareos \
     "$base_pki/public/Bareos/Rsa/bareos-dir_rsa.crt" \
-    "$base_pki/public/Bareos/Ecdsa/bareos-dir_ecdsa.crt" \
     "$cible":"$base_bareos"/Cert/dir/
 
-# SD
+# SD LOCAL
 rsync -e ssh --no-p --chmod=F600 --chown=bareos:bareos \
-    "$base_pki/private/Bareos/Rsa/bareos-sd_rsa.key" \
-    "$base_pki/private/Bareos/Ecdsa/bareos-sd_ecdsa.key" \
-    "$cible":"$base_bareos"/Keys/sd/
+    "$base_pki/private/Bareos/Rsa/bareos-sd-local_rsa.key" \
+    "$cible":"$base_bareos"/Keys/sd/local/
 rsync -e ssh --no-p --chmod=F644 --chown=bareos:bareos \
-    "$base_pki/public/Bareos/Rsa/bareos-sd_rsa.crt" \
-    "$base_pki/public/Bareos/Ecdsa/bareos-sd_ecdsa.crt" \
-    "$cible":"$base_bareos"/Cert/sd/
+    "$base_pki/public/Bareos/Rsa/bareos-sd-local_rsa.crt" \
+    "$cible":"$base_bareos"/Cert/sd/local/
+
+# SD REMOTE
+rsync -e ssh --no-p --chmod=F600 --chown=bareos:bareos \
+    "$base_pki/private/Bareos/Rsa/bareos-sd-remote_rsa.key" \
+    "$cible":"$base_bareos"/Keys/sd/remote/
+rsync -e ssh --no-p --chmod=F644 --chown=bareos:bareos \
+    "$base_pki/public/Bareos/Rsa/bareos-sd-remote_rsa.crt" \
+    "$cible":"$base_bareos"/Cert/sd/remote/
 
 # FD
 rsync -e ssh --no-p --chmod=F600 --chown=bareos:bareos \
     "$base_pki/private/Bareos/Rsa/bareos-fd_rsa.key" \
-    "$base_pki/private/Bareos/Ecdsa/bareos-fd_ecdsa.key" \
     "$cible":"$base_bareos"/Keys/fd/
 rsync -e ssh --no-p --chmod=F644 --chown=bareos:bareos \
     "$base_pki/public/Bareos/Rsa/bareos-fd_rsa.crt" \
-    "$base_pki/public/Bareos/Ecdsa/bareos-fd_ecdsa.crt" \
     "$cible":"$base_bareos"/Cert/fd/
 
 # WEBUI
 rsync -e ssh --no-p --chmod=F600 --chown=bareos:bareos \
     "$base_pki/private/Bareos/Rsa/bareos_rsa.key" \
-    "$base_pki/private/Bareos/Ecdsa/bareos_ecdsa.key" \
     "$cible":"$base_bareos"/Keys/web/
 rsync -e ssh --no-p --chmod=F644 --chown=bareos:bareos \
     "$base_pki/public/Bareos/Rsa/bareos_rsa.crt" \
-    "$base_pki/public/Bareos/Ecdsa/bareos_ecdsa.crt" \
     "$cible":"$base_bareos"/Cert/web/
 
 # POSTGRESQL
 rsync -e ssh --no-p --chmod=F640 --chown=postgres:postgres \
     "$base_pki/private/PostGreSQL/Rsa/postgresql_rsa.key" \
-    "$base_pki/private/PostGreSQL/Ecdsa/postgresql_ecdsa.key" \
     "$cible":"$base_bareos"/Keys/post/
 rsync -e ssh --no-p --chmod=F644 --chown=postgres:postgres \
     "$base_pki/public/PostGreSQL/Rsa/postgresql_rsa.crt" \
-    "$base_pki/public/PostGreSQL/Ecdsa/postgresql_ecdsa.crt" \
     "$cible":"$base_bareos"/Cert/post/
+
+# CLIENT WIN
+rsync -e ssh --no-p --chmod=F600 --chown=bareos:bareos \
+    "$base_pki/private/Bareos/Rsa/win_rsa.key" \
+    "$cible":"$base_bareos"/Keys/client/win/
+rsync -e ssh --no-p --chmod=F644 --chown=bareos:bareos \
+    "$base_pki/public/Bareos/Rsa/win_rsa.crt" \
+    "$cible":"$base_bareos"/Cert/client/win/
+
+# CLIENT LIN
+rsync -e ssh --no-p --chmod=F600 --chown=bareos:bareos \
+    "$base_pki/private/Bareos/Rsa/lin_rsa.key" \
+    "$cible":"$base_bareos"/Keys/client/lin/
+rsync -e ssh --no-p --chmod=F644 --chown=bareos:bareos \
+    "$base_pki/public/Bareos/Rsa/lin_rsa.crt" \
+    "$cible":"$base_bareos"/Cert/client/lin/
 
 # ===== PI =====
 cible="sednal@192.168.0.241"
@@ -169,9 +205,7 @@ ssh "$cible" "rm -f $base_vps/Keys/* $base_vps/Cert/*"
 
 rsync -e ssh --no-p --chmod=F600 --chown=debian:debian \
     "$base_pki/private/VPS/Rsa/vps_rsa.key" \
-    "$base_pki/private/VPS/Ecdsa/vps_ecdsa.key" \
     "$cible":"$base_vps"/Keys/
 rsync -e ssh --no-p --chmod=F644 --chown=debian:debian \
     "$base_pki/public/VPS/Rsa/vps_rsa.crt" \
-    "$base_pki/public/VPS/Ecdsa/vps_ecdsa.crt" \
     "$cible":"$base_vps"/Cert/
