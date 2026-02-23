c1535fcc92c266fd6b7198ab3ecbe69d7225106e
landes martin <landes_martin@yahoo.fr>
2026-02-23 07:25:02 +0000

    Update ajout_service.sh

--- PERSO/VAULT/SCRIPT/PKI/ajout_service.sh
+++ PERSO/VAULT/SCRIPT/PKI/ajout_service.sh
@@ -1,15 +1,12 @@
 #!/bin/bash
 
 # === AJOUTER === Nom Service
-# Tableau des services en minuscule
 services=()
 
 # === AJOUTER === Cible Certificats
-# Où l'on va copier les certificats : Format => user@IP
 cible=""
 
-# === AJOUTER === Nom Service
-# Dossier de destination sur la machine cible (exemple /etc/bareos)
+# === AJOUTER === Dossier de destination sur la machine cible
 base_service="/etc/"
 
 # Variable à ne pas toucher
@@ -19,8 +16,17 @@ domain=".sednal.lan"
 set -e
 
 path() {
-    local svc="$1"
-    echo "${svc^}"
+    if [[ "$1" == "proxmox" ]]; then echo "Proxmox"
+    elif [[ "$1" == "pihole" ]]; then echo "Pihole"
+    elif [[ "$1" == "upsnap" ]]; then echo "Upsnap"
+    elif [[ "$1" == "infra" ]]; then echo "Infra"
+    elif [[ "$1" == bareos* ]]; then echo "Bareos"
+    elif [[ "$1" == "win" ]]; then echo "Bareos"
+    elif [[ "$1" == "lin" ]]; then echo "Bareos"
+    elif [[ "$1" == "postgresql" ]]; then echo "PostGreSQL"
+    elif [[ "$1" == "vps" ]]; then echo "VPS"
+    elif [[ "$1" == "cockpit" ]]; then echo "Cockpit"
+    fi
 }
 
 # === GENERATION CERTIFICATS ===
@@ -28,21 +34,17 @@ for svc in "${services[@]}"; do
     cert="${svc}${domain}"
     folder=$(path "$svc")
 
-    # Chemin par defaut sur 192.168.0.238 : /etc/Vault/PKI/ Nom du service avec une majuscule 
     mkdir -p "$base_pki"/{private,public}/"$folder"/{Rsa,Ecdsa}
 
-    # Génération RSA
     echo "Génération RSA : $cert => $folder"
     result=$(vault write -format=json PKI_Sednal_Inter_RSA/issue/Cert_Inter_RSA common_name="$cert")
     echo "$result" | jq -r '.data.certificate' | sudo tee "$base_pki/public/$folder/Rsa/${svc}_rsa.crt" > /dev/null
     echo "$result" | jq -r '.data.private_key'  | sudo tee "$base_pki/private/$folder/Rsa/${svc}_rsa.key" > /dev/null
 
-    # Génération ECDSA
     echo "Génération ECDSA : $cert => $folder"
     result=$(vault write -format=json PKI_Sednal_Inter_ECDSA/issue/Cert_Inter_ECDSA common_name="$cert")
     echo "$result" | jq -r '.data.certificate' | sudo tee "$base_pki/public/$folder/Ecdsa/${svc}_ecdsa.crt" > /dev/null
     echo "$result" | jq -r '.data.private_key'  | sudo tee "$base_pki/private/$folder/Ecdsa/${svc}_ecdsa.key" > /dev/null
-
 done
 
 # Réappliquer les droits sur Vault
@@ -53,8 +55,7 @@ chown -R vault:vault "$base_pki"
 # === DEPLOIEMENT ===
 for svc in "${services[@]}"; do
     folder=$(path "$svc")
-     
-     # Chemin par defaut : /etc/[NOM SERVICE]/
+
     ssh "$cible" "mkdir -p $base_service/{Cert,Keys,CA}"
 
     rsync -e ssh --no-p --chmod=F600 --chown=sednal:sednal \
@@ -66,7 +67,6 @@ for svc in "${services[@]}"; do
         "$base_pki/public/$folder/Rsa/${svc}_rsa.crt" \
         "$base_pki/public/$folder/Ecdsa/${svc}_ecdsa.crt" \
         "$cible":"$base_service/Cert/"
-
 done
 
 # CA
