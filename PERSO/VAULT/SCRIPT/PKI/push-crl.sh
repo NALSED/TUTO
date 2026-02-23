87d50ccb6add6708256e40d984d8074ffa609965
landes martin <landes_martin@yahoo.fr>
2026-02-23 14:40:07 +0000

    Update push-crl.sh

--- PERSO/VAULT/SCRIPT/PKI/push-crl.sh
+++ PERSO/VAULT/SCRIPT/PKI/push-crl.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-curl -s https://vault.sednal.lan:8200/v1/PKI_Sednal_Root_RSA/crl \
+curl -s http://vault.sednal.lan:8200/v1/PKI_Sednal_Root_RSA/crl \
     -o /tmp/root_r.crl
-curl -s https://vault.sednal.lan:8200/v1/PKI_Sednal_Root_ECDSA/crl \
+curl -s http://vault.sednal.lan:8200/v1/PKI_Sednal_Root_ECDSA/crl \
     -o /tmp/root_e.crl
-curl -s https://vault.sednal.lan:8200/v1/PKI_Sednal_Inter_RSA/crl \
+curl -s http://vault.sednal.lan:8200/v1/PKI_Sednal_Inter_RSA/crl \
     -o /tmp/intermediate_r.crl
-curl -s https://vault.sednal.lan:8200/v1/PKI_Sednal_Inter_ECDSA/crl \
+curl -s http://vault.sednal.lan:8200/v1/PKI_Sednal_Inter_ECDSA/crl \
     -o /tmp/intermediate_e.crl
 
 scp /tmp/root_r.crl /tmp/root_e.crl \
