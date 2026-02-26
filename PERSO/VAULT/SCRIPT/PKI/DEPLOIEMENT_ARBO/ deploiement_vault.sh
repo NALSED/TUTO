ef7ef28fbeb8f62ec0a989db7846c87b2cd04899
landes martin <landes_martin@yahoo.fr>
2026-02-26 16:16:27 +0400

    Update  deploiement_vault.sh

--- PERSO/VAULT/SCRIPT/PKI/DEPLOIEMENT_ARBO/ deploiement_vault.sh
+++ PERSO/VAULT/SCRIPT/PKI/DEPLOIEMENT_ARBO/ deploiement_vault.sh
@@ -12,6 +12,9 @@ services=(bareos cockpit infra pihole postgresql proxmox upsnap vps)
 
 set -e
 
+# sednal dans le groupe vault
+usermod -aG vault sednal
+
 # === Création de l'arborescence ===
 for s in "${services[@]}"; do
     mkdir -p "$base/private/$s"/{rsa,ecdsa}
@@ -33,6 +36,6 @@ find "$base/public"  -type d -exec chmod 755 {} \;
 chmod 755 "$base/cert_ca"
 chmod 755 "$base/cert_ca/inter"
 chmod 755 "$base/cert_ca/root"
-chmod 700 "$base/cert_ca/csr"
+chmod 755 "$base/cert_ca/csr"
 
 echo "Arborescence Vault créée et droits appliqués "
