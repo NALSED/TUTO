428574eeb21b4e387260e7b64af213a5763a3be4
landes martin <landes_martin@yahoo.fr>
2026-02-26 09:31:36 +0000

    Update deploiement_infra.sh

--- PERSO/VAULT/SCRIPT/PKI/DEPLOIEMENT_ARBO/deploiement_infra.sh
+++ PERSO/VAULT/SCRIPT/PKI/DEPLOIEMENT_ARBO/deploiement_infra.sh
@@ -7,18 +7,20 @@
 set -e
 
 # === Création de l'arborescence ===
-mkdir -p /etc/infra/{CA,Cert,Keys}
+mkdir -p /etc/infra/ssl/{ca,cert,keys}
 
 # === Propriétaire ===
-chown root:root   /etc/infra
-chown sednal:sednal /etc/infra/CA
-chown sednal:sednal /etc/infra/Cert
-chown sednal:sednal /etc/infra/Keys
+chown root:root     /etc/infra
+chown root:root     /etc/infra/ssl
+chown sednal:sednal /etc/infra/ssl/ca
+chown sednal:sednal /etc/infra/ssl/cert
+chown sednal:sednal /etc/infra/ssl/keys
 
 # === Droits ===
 chmod 755 /etc/infra
-chmod 755 /etc/infra/CA
-chmod 755 /etc/infra/Cert
-chmod 755 /etc/infra/Keys
+chmod 755 /etc/infra/ssl
+chmod 755 /etc/infra/ssl/ca
+chmod 755 /etc/infra/ssl/cert
+chmod 755 /etc/infra/ssl/keys
 
 echo "Arborescence Infra créée et droits appliqués"
